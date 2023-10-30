ed7e5e989385eeaa4a32c145ff4c7acdc6a9b442
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-30 19:43:45 +0800

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,164 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// 定义任务订单结构
-typedef struct {
-    
-   
-int start;
+// 定义一个结构体来表示任务订单
+  typedef struct {
+    int start;
     int end;
 } Order;
 
-// 比较函数，用于任务订单排序
-int compare(const void *a, const void *b) {
-    return ((Order*)a)->start - ((Order*)b)->start;
+  int compare_orders(const void *a, const void *b) {
+    // 按照结束时间升序排序
+  return ((Order*)a)->end - ((Order*)b)->end;
 }
 
-int main() {
-    Order orders[
-    Order orders
+  int main() {
+    Order orders[100];  // 假设最多有100个任务订单
 
-   
-100];  // 假设最多有100个订单
-    int n = 0;  // 订单数量
+  int n = 0;
+// 读取输入
+  while (scanf("%d %d", &orders[n].start, &orders[n].end) == 2) {n++;
+  }
+// 根据任务订单的结束时间升序排序
 
-    // 读取任务订单
-    
-   
-while (scanf("%d %d", &orders[n].start, &orders[n].end) == 2) {
-        n++;
-    }
+  sizeof(Order), compare_orders);
 
-    
-        n++;
-    }
+  int passenger_count = 1;
 
-   
+  int current_end_time = orders[0].end;
 
-        n++;
-// 对订单按照出发时间排序
-    qsort(orders, n, 
-    qsort(orders, n,
+    // 打印第一个任务订单
 
-   
-sizeof(Order), compare);
+  printf("%d %d ", orders[0].start, orders[0].end);
 
-    int drivers[100] = {0};  // 司机的时间表，0 表示空闲
-    int num_drivers = 0;  // 司机数量
+// 打印时间表
 
-    // 处理订单
-    
-   
-for (int i = 0; i < n; i++) {
-        int assigned = 0;
-        for (int j = 0; j < num_drivers; j++) {
-            if (drivers[j] <= orders[i].start) {
-                drivers[j] = orders[i].end;
-                assigned = 
-                drivers[j] = orders[i].end;
-                assigned
+  for (int i = 1; i < n; i++) {
+    if (orders[i].start >= current_end_time) {
 
-                drivers[j] = orders[i].end;
+// 可以派同一个旅客继续执行
 
-                drivers[j] = orders[i].
-
-                drivers[j] = orders
-
-                drivers[j]
-
-                drivers
-
-               
-1;
-                break;
-            }
+  printf("%d %d ", orders[i].start, orders[i].end);
+            current_end_time = orders[i].end;
         }
-        
-            }
-       
-if (!assigned) {
-            drivers[num_drivers] = orders[i].end;
-            num_drivers++;
+   else {
+  passenger_count++;
+// 需要新的旅客
+
+
+  printf("\n%d %d ", orders[i].start, orders[i].end);
+            current_end_time = orders[i].end;
         }
     }
 
-    
-            drivers[num_drivers] = orders[i].end;
-            num_drivers++;
-        }
-    }
 
-   
-
-            drivers[num_drivers] = orders[i].end;
-            num_drivers++;
-        }
-   
-
-            drivers[num_drivers] = orders[i].end;
-            num_drivers++;
-       
-
-            drivers[num_drivers] = orders[i].end;
-            num_dr
-
-            drivers[num_drivers] = orders[i].end;
-
-            drivers[num_drivers] = orders[i].end
-
-            drivers[num_drivers] = orders[i
-
-            drivers[num_drivers]
-
-            drivers[num_drivers
-
-            drivers[num
-// 输出结果
-    printf("%d\n", num_drivers);
-    
-   
-for (int i = 0; i < num_drivers; i++) {
-        printf("Driver %d:", i + 1);
-        for (int j = 0; j < n; j++) {
-            
-           
-if (drivers[i] >= orders[j].end) {
-                printf(" %d", orders[j].start);
-                drivers[i] = orders[j].start;
-            }
-        }
-        
-                drivers[i] = orders[j].start;
-            }
-        }
-
-                drivers[i] = orders[j].start;
-            }
-       
-
-                drivers[i] = orders[j].start;
-            }
-
-                drivers[i] = orders[j].start
-
-                drivers[i] = orders
-
-                drivers[i] =
-
-                drivers[i
-
-               
-printf(" %d\n", drivers[i]);
-    }
-
-    
-    }
-
-   
-
-    }
+// 打印最少旅客需求数
+    printf("\n%d\n", passenger_count);
 
 return 0;
-}
-
 }
