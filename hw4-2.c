bc7cdf227fd51294a68b78b6c662dd49992e9d04
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-30 09:04:35 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define ROW 6
+#include <string.h>
 
-
-  struct Account {
-  int accountNumber;
-
-  int password;
-
-  double balance;
+// 創建帳戶資訊的二維陣列
+char account_info[][3] = {
+    {"123", "456", "9000"},
+    {"456", "789", "5000"},
+    {"789", "888", "6000"},
+    {
+   
+"336", "558", "10000"},
+    {
+   
+"775", "666", "12000"},
+    {"566", "221", "7000"}
 };
-  main(){
-  struct Account accounts[ROW] ={{123,456,9000},{456,789,5000},{ 789,888,6000},
-    { 336,558,10000},{ 775,666,12000},{ 566,221,7000}};
 
-
-  int n;
-  scanf ("%d",&n);
-
-  for (int i=0;i<n ;i++){
-  int accountNumber, password;
-  int accountIndex = -1;
-
-  for (int i=0;i<ROW;i++){
-  if (accounts[i].accountNumber == accountNumber && accounts[i].password == password){
-  break;
-  }
-
-
-  if (accountIndex != -1) {
-  printf("%d\n", accounts[accountIndex].balance);
-  }
-
-  else {
-  printf("error\n");
+int check_balance(char *username, char *password) {
+    
+   
+for (int i = 0; i < sizeof(account_info) / sizeof(account_info[0]); i++) {
+        if (strcmp(account_info[i][0], username) == 0 && strcmp(account_info[i][1], password) == 0) {
+            return atoi(account_info[i][2]);
+        }
+    }
+    return -1;
 }
-  }
-  }
 
-  }
+int main() {
+    int N;
+    printf("請輸入測試案例數目N: ");
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        char username[20];
+        char password[20];
+
+        printf("請輸入帳號: ");
+        scanf("%s", username);
+        printf("請輸入密碼: ");
+        scanf("%s", password);
+
+        int balance = check_balance(username, password);
+        if (balance != -1) {
+            printf("帳戶餘額為: $%d\n", balance);
+        } else {
+            printf("error\n");
+        }
+    }
+
+    return 0;
+}
