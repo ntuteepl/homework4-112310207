56f26f4b4a26fd01d1824257b1ba0d7767674a8f
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-31 00:16:20 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,73 +1,56 @@
 #include <stdio.h>
 
 
-int accounts[][3] = {
+  int accounts[][3] = {
     {123, 456, 9000},
-
-    {
-456, 789, 5000},
-    {
-
-789, 888, 6000},
+    {456, 789, 5000},
+    {789, 888, 6000},
     {336, 558, 10000},
+    {775, 666, 12000},
+    {566, 221, 7000}};
 
-    {
-775, 666, 12000},
-
-    {
-566, 221, 7000}
-};
-
-int main() {
+  int main() {
 
 
-int N;
+  int N;
     scanf("%d", &N);
 
 
 
-for (int i = 0; i < N; i++){
+  for (int i = 0; i < N; i++){
 
 
-int accountNumber, password;
+  int accountNumber, password;
 
 
-scanf("%d %d", &accountNumber, &password);
-
-
-
-int found = 0;
-        int balance = 0;
+  scanf("%d %d", &accountNumber, &password);
 
 
 
-for (int j = 0; j < 6; j++) {
-            if (accounts[j][0] == accountNumber && accounts[j][1] == password) {
-                found = 1;
-                balance = accounts[j][2];
+  int found = 0;
+  int balance = 0;
 
 
-break;
-            }
-        }
+  for (int j = 0; j < 6; j++) {
+  if (accounts[j][0] == accountNumber && accounts[j][1] == password) {
+       found = 1;
+       balance = accounts[j][2];
 
 
-
-
-
-
-
-
-if (found) {
-
-
-printf("%d\n", balance);
-        }
-        else {
-
-
-printf("error\n");
-        }
-
+  break;
+ }
 }
+
+  if (found) {
+
+
+  printf("%d\n", balance);
 }
+  else {
+
+
+  printf("error\n");
+}
+
+  }
+    }
