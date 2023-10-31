e2b2373916dda3dafcc095eb5b13c0b119dd8328
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-31 12:58:41 +0800

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,100 +1,71 @@
+#define ARRAYSIZE 100
+
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int i=0,j=0,k=0,x=0,y=1,sum=0,num=0,s[100],d[100],sf[100],df[100],s1,d1,count=0;
-    while(scanf("%d%d",&s[i],&d[i])!=EOF)  //輸入
-    {
-        i++;
-        count++;
+int main() {
+    int n = 0;
+    int sd[ARRAYSIZE][2];
+    int t[ARRAYSIZE][2];
+    int cars = 0; // 初始化为0
+    while (scanf("%d %d", &sd[n][0], &sd[n][1]) != EOF) {
+        n++;
     }
 
-    for(i=0;i<count-1;i++)             //整理班表
-    {
-        for(int j=0;j<count;j++)
-        {
-            if(s[j]>s[j+1])
-            {
-                s1=s[j];
-                s[j]=s[j+1];
-                s[j+1]=s1;
-
-                d1=d[j];
-                d[j]=d[j+1];
-                d[j+1]=d1;
+    // 冒泡排序
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (sd[j][0] > sd[j + 1][0] || (sd[j][0] == sd[j + 1][0] && sd[j][1] > sd[j + 1][1])) {
+                // Swap the rows if the first column element is out of order
+                int temp1 = sd[j][0];
+                int temp2 = sd[j][1];
+                sd[j][0] = sd[j + 1][0];
+                sd[j][1] = sd[j + 1][1];
+                sd[j + 1][0] = temp1;
+                sd[j + 1][1] = temp2;
             }
         }
     }
+     for (int i = 0; i < n; i++)
+     {
+         t[i][0]=sd[i][0];
+         t[i][1]=sd[i][1];
+     }
 
-    for(i=0;i<count;i++)        //備份
-    {
-        sf[i]=s[i];
-        df[i]=d[i];
-    }
 
-    while(y!=0)                          //幾台車
-    {
-        for(i=0;i<count;i++)
-        {
-            sum=0;
+    for (int i = 0; i < n; i++) {
+        if (sd[i][0] != 0) { // Skip already processed rows
+            cars++;
+            int current_time = sd[i][1]; // 当前司机的时间表
 
-            if(d[x]<=s[i])
-            {
-                x=i;
-                s[i]=0;
-            }
-            s[x]=0;
-
-            if(i==count-1)
-            {
-                num++;
-                for(j=0;j<count;j++)
-                {
-                    if(s[j]!=0)
-                    {
-                        x=j;
-                        j=count;
-                    }
-                }
-            }
-
-            for(k=0;k<count;k++)
-            sum=sum+s[k];
-            if(sum==0)
-            y=0;
-        }
-    }
-
-    printf("%d",num);
-    x=0;
-
-    for(i=0;i<num;i++)            //輸出班表
-    {
-        printf("\nDriver %d's schedule is %d %d ",i+1,sf[x],df[x]);;
-
-        for(j=0;j<count;j++)
-        {
-            if(df[x]<=sf[j])
-            {
-                x=j;
-                printf("%d %d ",sf[j],df[j]);
-                sf[j]=0;
-            }
-            sf[x]=0;
-
-            if(j==count-1)
-            {
-                for(k=0;k<count;k++)
-                {
-                    if(sf[k]!=0)
-                    {
-                        x=k;
-                        k=count;
-                    }
+            for (int j = i + 1; j < n; j++) {
+                if (sd[j][0] >= current_time) {
+                    // 当前任务可以分配给当前司机
+                    current_time = sd[j][1];
+                    sd[j][0] = 0; // 将任务标记为已分配
                 }
             }
         }
     }
+
+   printf("%d\n", cars);
+   cars=0;
+    for (int i = 0; i < n; i++) {
+        if (t[i][0] != 0) { // Skip already processed rows
+            cars++;
+            printf("Driver %d's schedule is %d %d ", cars, t[i][0], t[i][1]);
+
+            for (int j = i + 1; j < n; j++) {
+                if (t[j][0] >= t[i][1]) {
+                    // 当前任务可以分配给当前司机
+                    printf("%d %d ", t[j][0], t[j][1]);
+                    t[i][1] = t[j][1];
+                    t[j][0]=0; // 更新当前司机的结束时间
+                }
+            }
+            printf("\n");
+        }
+    }
+
     return 0;
 }
+
